a85b93cef346e29b69a60712760a584ecbb8fb73
Jeff Hostetler <hostetjm@gmail.com>
2018-01-27 10:49:44 -0700

    Create a simple Gemfile when building a new project

--- spec/goby/scaffold_spec.rb
+++ spec/goby/scaffold_spec.rb
@@ -2,28 +2,27 @@ require 'goby'
 require 'fileutils'
 
 RSpec.describe Scaffold do
+  let(:project) {"some-goby-project"}
 
   context "simple" do
+    after do
+      # Clean up the scaffolding.
+      FileUtils.remove_dir project
+    end
     it "should create the appropriate directories & files" do
-
-      project = "goby-project"
       Scaffold::simple project
 
       # Ensure all of the directories exist.
       expect(Dir.exists? "#{project}").to be true
       [ '', 'battle', 'entity',
-      'event', 'item', 'map' ].each do |dir|
+        'event', 'item', 'map' ].each do |dir|
         expect(Dir.exist? "#{project}/src/#{dir}").to be true
       end
 
       # Ensure all of the files exist.
-      [ '.gitignore', 'src/main.rb', 'src/map/farm.rb' ].each do |file|
+      [ '.gitignore', 'Gemfile', 'src/main.rb', 'src/map/farm.rb' ].each do |file|
         expect(File.exist? "#{project}/#{file}").to be true
       end
-
-      # Clean up the scaffolding.
-      FileUtils.remove_dir project
-
     end
   end
 
